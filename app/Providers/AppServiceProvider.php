b256608cdb3541cb626c88ef904bcae510f6e088
Jeans <sistemadesignstyle@gmail.com>
2023-04-30 22:12:51 -0500

    test desarrollo laravel vite

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,9 +28,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        if($this->app->environment('production')) {
+       /*  if($this->app->environment('production')) {
             \URL::forceScheme('https');
-        }
+        } */
         
     }
 }
