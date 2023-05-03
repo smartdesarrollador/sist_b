714c15515ed8329f4dfd021016523adf11c90220
smart <sistemadesignstyle@gmail.com>
2023-05-03 16:28:55 -0500

    megamenu tailwind

--- resources/views/tienda_peruvian/layouts/layout.blade.php
+++ resources/views/tienda_peruvian/layouts/layout.blade.php
@@ -1,18 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>@yield('titulo')</title>
 
-       <meta charset="UTF-8">
-       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-       <title>Document</title>
-      
+<head>
+    <title>@yield('titulo')</title>
 
-       @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body>
-    
-        @yield('contenido')
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 
-    </body>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    {{--  1 - Megamenu --}}
+    <link rel="stylesheet" href="{{ asset('assets_tienda/css/megamenu.css') }}">
+    {{--  /1 - Megamenu --}}
+</head>
+
+<body>
+    {{--  2 - Megamenu --}}
+    @include('tienda_peruvian.layouts.megamenu')
+    {{--  /2 - Megamenu --}}
+
+    @yield('contenido')
+
+</body>
+
 </html>
