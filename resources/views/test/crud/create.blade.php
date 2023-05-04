70d77c4d864f42076fe02357d22a29b86ae0a295
smart <sistemadesignstyle@gmail.com>
2023-05-04 17:44:48 -0500

    colocando estilos a create del Crud

--- resources/views/test/crud/create.blade.php
+++ resources/views/test/crud/create.blade.php
@@ -4,14 +4,46 @@
 
 @section('contenido')
 
-    <h1>Crud</h1>
+    <div class="container mx-auto">
+        <h2>Crear</h2>
+        <br>
 
-    <form action="{{ route('crud.store') }}" method="POST">
-        @csrf
-        <input type="text" name="nombre" value="">
-        <input type="text" name="imageUrl" value="">
-        <button type="submit">Send</button>
 
-    </form>
+
+        <form action="{{ route('crud.store') }}" method="POST">
+            @csrf
+            <div class="flex flex-col">
+                <label for="nombre">Nombre</label>
+                <input type="text" name="nombre" value="">
+            </div>
+            <div class="flex flex-col">
+                <label for="texto">nombre img</label>
+                <input type="text" name="imageUrl" value="">
+            </div>
+            <br>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                type="submit">Enviar</button>
+
+        </form>
+        <br>
+        <br>
+
+        <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            href="{{ route('crud.index') }}">Regresar</a>
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @stop
